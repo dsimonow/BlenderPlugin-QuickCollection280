f4314c5c4b07c3c942a49a8371660b7a7037be44
Dimitri <dimitrisimonow90@googlemail.com>
2019-08-04 18:25:06 +0200

    improvements

--- quickcollection.py
+++ quickcollection.py
@@ -14,17 +14,20 @@ class QuickCollectionManager(GizmoGroup):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'WINDOW'
     bl_options = {'PERSISTENT', 'SCALE'}
+    buttonNumber = 20
+    
    
     @classmethod
     def poll(cls, context):
-       
         ob = context.object
         return ob
    
     def setup(self, context):
         buttons = []
         counter = 1
-        for i in range(16):
+        
+        # Amount of buttons
+        for i in range(self.buttonNumber):
             button = self.gizmos.new("GIZMO_GT_button_2d")
             buttons.append(button)
            
@@ -34,16 +37,14 @@ class QuickCollectionManager(GizmoGroup):
  
             #button.alpha = 0.0
             button.color = 0.5,0.5,0.5
-            button.color_highlight = 0.8, 0.8, 0.8
             button.alpha_highlight = 1
            
             promp = button.target_set_operator("object.hide_collection")
             promp.collection_index = counter
             promp.toggle = True
            
-           
-            #drawselect = button.draw
-            button.scale_basis = 3 # Same as buttons defined in C
+           # but size. if changed draw_prepare "runningoffset" needs adjustment
+            button.scale_basis = 3 
             counter = counter + 1
            
         self.buttonsfoo = buttons
@@ -51,31 +52,37 @@ class QuickCollectionManager(GizmoGroup):
     def draw_prepare(self, context):
         region = context.region
         lButtons = self.buttonsfoo
+        coll = context.view_layer.layer_collection.children
+        
+        perRow = 5
+        offsetwidth = 0
+        offsetheight = 0
+        runningoffset = 7
        
         counter = 0
-        i = 0
-        j = 0
         for but in lButtons:
-            but.matrix_basis[0][3] = (region.width / 2 )+j + (counter%4 * 7)
-            but.matrix_basis[1][3] = region.height - 33 - i
-            counter = counter + 1
-           
-            if counter%4 == 0 and j == 5*7 and i == 0:
-                i = 7
-            if counter%4 == 0 and i == 7 and j == 0:
-                j = 5*7
-                i = 0
-            if counter%4 == 0 and i == 0 and j == 0:
-                i = 7    
-    def invoke_prepare(self,context, gizmo):
-        lButtons = self.buttonsfoo
-        gizmoIndex = lButtons.index(gizmo)
-        coll = context.view_layer.layer_collection.children
-       
-        if not (gizmoIndex > len(coll)):
-            if coll[gizmoIndex].is_visible == True:
-                lButtons[gizmoIndex].color = 0.1,0.1,0.1
-            if coll[gizmoIndex].is_visible == False:
-                lButtons[gizmoIndex].color = 0.5,0.5,0.5
+            
+            if counter%perRow == 0 and counter != 0:
+                offsetheight = runningoffset
+                
+            if counter == len(lButtons)/2:
+                # probably if more buttons are added. the + perRow needs to be multiplied 
+                offsetwidth = perRow * runningoffset + perRow
+                offsetheight = 0
+                
+            but.matrix_basis[0][3] = (region.width/2) + offsetwidth + ((counter%perRow) * runningoffset)
+            but.matrix_basis[1][3] = (region.height - 33) - offsetheight
+                
+            counter = counter +1
+            
+        # Might be a performance hog. 
+        collCounter = 0
+        for c in coll:
+            if c.is_visible == False:
+                lButtons[collCounter].color = 0.1,0.1,0.1
+            if c.is_visible == True:
+                lButtons[collCounter].color = 0.5,0.5,0.5
+                
+            collCounter = collCounter + 1
        
 bpy.utils.register_class(QuickCollectionManager)
